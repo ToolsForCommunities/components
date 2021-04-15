69bad4f08c7f2b38f1c10e25f82942b9c66c8566
CodingCarlos <hola@codingcarlos.com>
2021-04-15 18:48:50 +0200

    Fix a11y plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
   // stories: ['../src/**/*.stories.js'],
   stories: ['./stories/**/*.stories.js'],
+  addons: ['@storybook/addon-a11y'],
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,8 @@
 import { addDecorator } from '@storybook/vue'
-import { withA11y } from '@storybook/addon-a11y'
 import { withKnobs } from '@storybook/addon-knobs'
 import { withTemplate } from '~storybook/addon-show-vue-markup'
 import { withVuetify } from '~storybook/addon-vuetify'
 
-// addDecorator(withA11y)
 addDecorator(withKnobs)
 addDecorator(withTemplate)
 addDecorator(withVuetify)
@@ -14,6 +12,6 @@ export const parameters = {
     element: '#root',
     config: {},
     options: {},
-    manual: true,
+    manual: false,
   },
 };
