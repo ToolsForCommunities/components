f137042274cb8c81102fc7d2939652cf1d85f309
CodingCarlos <hola@codingcarlos.com>
2021-04-15 18:41:50 +0200

    Update deprecated options in storybook

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // stories: ['../src/**/*.stories.js'],
+  stories: ['./stories/**/*.stories.js'],
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,19 @@
-// Imports
-import { configure, addDecorator } from '@storybook/vue'
+import { addDecorator } from '@storybook/vue'
 import { withA11y } from '@storybook/addon-a11y'
 import { withKnobs } from '@storybook/addon-knobs'
 import { withTemplate } from '~storybook/addon-show-vue-markup'
 import { withVuetify } from '~storybook/addon-vuetify'
 
-addDecorator(withA11y)
+// addDecorator(withA11y)
 addDecorator(withKnobs)
 addDecorator(withTemplate)
 addDecorator(withVuetify)
 
-configure(require.context('./stories', true, /\.stories\.js$/), module)
+export const parameters = {
+  a11y: {
+    element: '#root',
+    config: {},
+    options: {},
+    manual: true,
+  },
+};
